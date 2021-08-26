6b672bb5d1bbb54ea180636d9806ba46c32c5b33
Adee <adeewu@outlook.com>
2021-08-26 17:55:27 +0800

    添加CheckHelper

--- src/Adee.Store.Domain.Shared/Utils/Helpers/CheckHelper.cs
+++ src/Adee.Store.Domain.Shared/Utils/Helpers/CheckHelper.cs
@@ -1,66 +1,63 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Volo.Abp;
 
 namespace System
 {
-    public class AssertHelper
+    public class CheckHelper
     {
-        public static void IsNull(object obj, string name = "", string message = "")
+        public static void IsNull(object obj, string message = "", string name = "")
         {
             if (string.IsNullOrWhiteSpace(message))
             {
                 if (string.IsNullOrWhiteSpace(name))
                 {
-                    throw new Exception($"${nameof(name)}、{nameof(message)}必须提供一项");
+                    throw new UserFriendlyException ($"${nameof(name)}、{nameof(message)}必须提供一项");
                 }
 
                 message = $"{name}需要为空";
             }
-            if (obj != null)
+            if (obj == null) return;
+
+            var objType = obj.GetType();
+
+            if (objType == typeof(string) && string.IsNullOrWhiteSpace(obj.ToString()) == false)
             {
-                var objType = obj.GetType();
-                if (objType.IsArray())
-                {
-                    var anyMethod = typeof(Enumerable).GetMethod("Any");
-                    anyMethod.MakeGenericMethod(objType.GetGenericArguments());
+                throw new UserFriendlyException(message);
+            }
 
-                    var anyResult = (bool)anyMethod.Invoke(null, new object[] { obj });
-                    if (!anyResult) return;
-                }
-
-                throw new Exception(message);
+            if (objType.IsArray())
+            {
+                if (((ICollection)obj).Count > 0) throw new UserFriendlyException(message);
             }
         }
 
-        public static void IsNotNull(object obj, string name = "", string message = "")
+        public static void IsNotNull(object obj, string message = "", string name = "")
         {
             if (string.IsNullOrWhiteSpace(message))
             {
                 if (string.IsNullOrWhiteSpace(name))
                 {
-                    throw new Exception($"${nameof(name)}、{nameof(message)}必须提供一项");
+                    throw new UserFriendlyException($"${nameof(name)}、{nameof(message)}必须提供一项");
                 }
 
                 message = $"{name}不能为空";
             }
-            if (obj == null) throw new Exception(message);
+            if (obj == null) throw new UserFriendlyException(message);
 
             var objType = obj.GetType();
 
             if (objType == typeof(string) && string.IsNullOrWhiteSpace(obj.ToString()))
             {
-                throw new Exception(message);
+                throw new UserFriendlyException(message);
             }
 
             if (objType.IsArray())
             {
-                var anyMethod = typeof(Enumerable).GetMethod("Any");
-                anyMethod.MakeGenericMethod(objType.GetGenericArguments());
-
-                var anyResult = (bool) anyMethod.Invoke(null, new object[] { obj });
-                if(!anyResult) throw new Exception(message);
+                if (((ICollection)obj).Count <= 0) throw new UserFriendlyException(message);
             }
         }
 
@@ -70,7 +67,7 @@ namespace System
             {
                 message = $"条件为真";
             }
-            if (condition != true) throw new Exception(message);
+            if (condition != true) throw new UserFriendlyException(message);
         }
 
         public static void IsFalse(bool condition, string message = "")
@@ -79,7 +76,7 @@ namespace System
             {
                 message = $"条件为假";
             }
-            if (condition != false) throw new Exception(message);
+            if (condition != false) throw new UserFriendlyException(message);
         }
 
         public static void AreEqual<T>(T t1, T t2, string t1Name = "", string t2Name = "", string message = "")
@@ -88,22 +85,22 @@ namespace System
             {
                 if (string.IsNullOrWhiteSpace(t1Name) && string.IsNullOrWhiteSpace(t2Name))
                 {
-                    throw new Exception($"name、{nameof(message)}必须提供一项");
+                    throw new UserFriendlyException($"name、{nameof(message)}必须提供一项");
                 }
 
                 if (string.IsNullOrWhiteSpace(t1Name))
                 {
-                    throw new Exception($"请提供{nameof(t1Name)}参数");
+                    throw new UserFriendlyException($"请提供{nameof(t1Name)}参数");
                 }
 
                 if (string.IsNullOrWhiteSpace(t2Name))
                 {
-                    throw new Exception($"请提供{nameof(t2Name)}参数");
+                    throw new UserFriendlyException($"请提供{nameof(t2Name)}参数");
                 }
 
                 message = $"{t1Name}和{t2Name}的值需要相等";
             }
-            if (!t1.Equals(t2)) throw new Exception(message);
+            if (!t1.Equals(t2)) throw new UserFriendlyException(message);
         }
 
         public static void AreNotEqual<T>(T t1, T t2, string t1Name = "", string t2Name = "", string message = "")
@@ -112,22 +109,22 @@ namespace System
             {
                 if (string.IsNullOrWhiteSpace(t1Name) && string.IsNullOrWhiteSpace(t2Name))
                 {
-                    throw new Exception($"name、{nameof(message)}必须提供一项");
+                    throw new UserFriendlyException($"name、{nameof(message)}必须提供一项");
                 }
 
                 if (string.IsNullOrWhiteSpace(t1Name))
                 {
-                    throw new Exception($"请提供{nameof(t1Name)}参数");
+                    throw new UserFriendlyException($"请提供{nameof(t1Name)}参数");
                 }
 
                 if (string.IsNullOrWhiteSpace(t2Name))
                 {
-                    throw new Exception($"请提供{nameof(t2Name)}参数");
+                    throw new UserFriendlyException($"请提供{nameof(t2Name)}参数");
                 }
 
                 message = $"{t1Name}和{t2Name}的值不能相等";
             }
-            if (t1.Equals(t2)) throw new Exception(message);
+            if (t1.Equals(t2)) throw new UserFriendlyException(message);
         }
     }
 }

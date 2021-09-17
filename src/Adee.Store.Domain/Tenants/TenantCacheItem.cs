c128dc4e0505814c3dda63c99bd8f2f87bc0cf7b
Adee <adeewu@outlook.com>
2021-09-18 00:04:48 +0800

    TenantCacheItem

---
+++ src/Adee.Store.Domain/Tenants/TenantCacheItem.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Adee.Store.Domain.Tenants
+{
+    public class TenantCacheItem
+    {
+        /// <summary>
+        /// 软件编号
+        /// </summary>
+        public string SoftwareCode { get; set; }
+    }
+}
